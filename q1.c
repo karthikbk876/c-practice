3cfac47b21f3e335b74c0e4ec5583ad85b3ff5d1
vivekpawar9005 <vivek.pawar@yappes.com>
2022-05-24 10:03:28 +0530

    first commit

---
+++ q1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+
+
+  for(int i=1 ; i<=6; ++i){
+   printf("%d\n", i);}
+  // int i = 3;
+  // while (i<=10){
+  //     printf("%d\n", i);
+  //     i++;
+  // }
+}
+  
